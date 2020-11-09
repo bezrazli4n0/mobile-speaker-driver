c350da621b05ea4f5f058fb1c56af3543745e14e
bezrazli4n0 <rpglovemmo@gmail.com>
2020-11-09 22:11:17 +0300

    Fixed build script for release version.

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ mkdir build
 cd build
 set CC=clang
 set CXX=clang++
-cmake -G "Unix Makefiles" ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON -G "Unix Makefiles" ..
 cmake --build .
 cd ..
--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 rm -rf build
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON ..
 cmake --build .
